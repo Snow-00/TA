48ceb11c70097f054365756b928a392190581288
Snow-00 <wariorwilly43@gmail.com>
2022-06-04 18:59:33 +0700

    python success

---
+++ csv_log.py
@@ -0,0 +1,30 @@
+import serial
+
+arduino_port = "/dev/ttyACM0" #serial port of Arduino
+baud = 19200 #arduino uno runs at 9600 baud
+fileName="dataset_pH.csv" #name of the CSV file generated
+
+ser = serial.Serial(arduino_port, baud)
+print("Connected to Arduino port:" + arduino_port)
+samples = 10 #how many samples to collect
+print_labels = False
+line = 0
+
+while line <= samples:
+    # incoming = ser.read(9999)
+    # if len(incoming) > 0:
+    if print_labels:
+        if line==0:
+            print("Printing Column Headers")
+        else:
+            print("Line " + str(line) + ": writing...")
+    getData=str(ser.readline())
+    data=getData[0:][:-2]
+    print(data)
+
+    file = open(fileName, "a")
+    file.write(data + "\n") #write data with a newline
+    line = line+1
+
+print("Data collection complete!")
+file.close()
