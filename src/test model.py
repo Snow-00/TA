72b7257f8fac162bd7dc33050156157a9de37a81
Michael MGP Simatupang <mgpmichael@gmail.com>
2022-08-01 18:06:00 +0700

    Adding Test Model Code

---
+++ src/test model.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+model_file = "latestModel_15" # name of the saved model file
+model = keras.models.load_model(model_file)
+input_data = np.array([[16.7, -3.4]])
+output = model.predict(input_data)
+print(output)
